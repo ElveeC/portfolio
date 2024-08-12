1db6c4f97c0645b0ce70d482fd2609f9276347ff
Elvira <elveechernenko@yandex.ru>
2024-08-12 14:50:31 +0300

    feat: add icons for acrylica and camera shop

--- src/components/PortfolioItem/PortfolioItem.jsx
+++ src/components/PortfolioItem/PortfolioItem.jsx
@@ -1,7 +1,15 @@
 import { useInView } from "react-intersection-observer";
 
 import BathIcon from "../BathIcon/BathIcon";
+import BathIcon2 from "../BathIcon2/BathIcon2";
 import SinkIcon from "../SinkIcon/SinkIcon";
+import BuildingIcon from "../BuildingIcon/BuildingIcon";
+import HotelIcon from "../HotelIcon/HotelIcon";
+import CameraIcon from "../CameraIcon/CameraIcon";
+import CameraIcon2 from "../CameraIcon2/CameraIcon2";
+import CameraIcon3 from "../CameraIcon3/CameraIcon3";
+import PhotoCameraIcon from "../PhotoCameraIcon/PhotoCameraIcon";
+import PhotoCameraIcon2 from "../PhotoCameraIcon2/PhotoCameraIcon2";
 import styles from "./PortfolioItem.module.scss";
 import cn from "classnames";
 
@@ -20,11 +28,34 @@ const PortfolioItem = ({ item }) => {
   return (
     <li ref={ref} className={styles["portfolio-item"]}>
       <div className={styles["portfolio-item__link-wrapper"]}>
-        {item.name === ItemType.ACRILICA && <BathIcon inView={inView} />}
-        {item.name === ItemType.ACRILICA && <SinkIcon inView={inView} />}
-        <a className={cn(styles["portfolio-item__link"], {
-                [styles["portfolio-item__link--animated"]]: inView,
-              })} href={item.link}>
+        {item.name === ItemType.ACRILICA && (
+          <>
+            <HotelIcon inView={inView} />
+            <BathIcon inView={inView} />
+            <SinkIcon inView={inView} />
+            <BathIcon2 inView={inView} />
+            <BuildingIcon inView={inView} />
+          </>
+        )}
+         {item.name === ItemType.GIVE5 && (
+          <>
+          </>
+        )}
+        {item.name === ItemType.CAMERA_SHOP && (
+          <>
+            <PhotoCameraIcon2 inView={inView} />
+            <CameraIcon2 inView={inView} />
+            <PhotoCameraIcon inView={inView} />
+            <CameraIcon inView={inView} />
+            <CameraIcon3 inView={inView} />
+          </>
+        )}
+        <a
+          className={cn(styles["portfolio-item__link"], {
+            [styles["portfolio-item__link--animated"]]: inView,
+          })}
+          href={item.link}
+        >
           <h3
             className={cn(styles["portfolio-item__title"], {
               [styles["portfolio-item__title--animated"]]: inView,
@@ -41,9 +72,11 @@ const PortfolioItem = ({ item }) => {
         </a>
       </div>
 
-      <div className={cn(styles["portfolio-item__inner"], {
-                [styles["portfolio-item__inner--animated"]]: inView,
-              })}>
+      <div
+        className={cn(styles["portfolio-item__inner"], {
+          [styles["portfolio-item__inner--animated"]]: inView,
+        })}
+      >
         <p className={styles["portfolio-item__description"]}>
           {item.description}
         </p>
