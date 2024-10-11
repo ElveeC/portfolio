2f34a1405c2a6f1e2c5a4d30d23d13da5e8c40f2
Elvira <elveechernenko@yandex.ru>
2024-10-11 22:04:33 +0300

    feat: add basename to browserrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { Routes, Route, /*BrowserRouter*/ HashRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import MainPage from "./pages/MainPage/MainPage";
 
 import "./App.css";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename="/portfolio/">
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="*" element={<MainPage />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
