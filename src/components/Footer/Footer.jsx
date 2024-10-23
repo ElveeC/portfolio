83c3bb29699c13653ad4e8b6e1efddfefc3e78a1
Elvira <elveechernenko@yandex.ru>
2024-10-23 23:35:22 +0300

    feat: add padding to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
+import styles from "./Footer.module.scss";
+
 const Footer = () => {
     return (
-        <footer>
+        <footer className={styles.footer}>
             <p>All the icons are made by</p>
         </footer>
     )
